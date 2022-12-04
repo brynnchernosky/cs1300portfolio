3f5ba5025c7ebe7498547ccd681699ed2d5da160
brynnchernosky <56202540+brynnchernosky@users.noreply.github.com>
2022-12-04 11:35:46 -0500

    content

--- src/App.tsx
+++ src/App.tsx
@@ -14,27 +14,41 @@ function App() {
       projectTitle: "Todaylist",
       projectDescription:
         "Redesigning the Craislist webpage for the modern era",
-      projectImage: "",
+      projectImage:
+        "https://th.bing.com/th/id/R.fb8415118774a6ace2dda4eb4ad48779?rik=IAXXIjVJoy2XMA&pid=ImgRaw&r=0",
     },
     {
       projectTitle: "Avocademy",
       projectDescription: "Online learning modules to teach UI/UX design",
-      projectImage: "",
+      projectImage:
+        "https://th.bing.com/th/id/R.fb8415118774a6ace2dda4eb4ad48779?rik=IAXXIjVJoy2XMA&pid=ImgRaw&r=0",
     },
     {
       projectTitle: "Sweet Designs",
       projectDescription:
         "Bakery webpage highlighting the patisserie's sweet offerings",
-      projectImage: "VanillaMacaron.png",
+      projectImage:
+        "https://th.bing.com/th/id/R.fb8415118774a6ace2dda4eb4ad48779?rik=IAXXIjVJoy2XMA&pid=ImgRaw&r=0",
     },
     {
       projectTitle: "PhySims",
       projectDescription:
         "Teaching algebra-based mechanics to high school and college students",
-      projectImage: "Cover4.png",
+      projectImage:
+        "https://th.bing.com/th/id/R.fb8415118774a6ace2dda4eb4ad48779?rik=IAXXIjVJoy2XMA&pid=ImgRaw&r=0",
     },
-    { projectTitle: "", projectDescription: "", projectImage: "" },
-    { projectTitle: "", projectDescription: "", projectImage: "" },
+    {
+      projectTitle: "",
+      projectDescription: "",
+      projectImage:
+        "https://th.bing.com/th/id/R.fb8415118774a6ace2dda4eb4ad48779?rik=IAXXIjVJoy2XMA&pid=ImgRaw&r=0",
+    },
+    {
+      projectTitle: "",
+      projectDescription: "",
+      projectImage:
+        "https://th.bing.com/th/id/R.fb8415118774a6ace2dda4eb4ad48779?rik=IAXXIjVJoy2XMA&pid=ImgRaw&r=0",
+    },
   ];
 
   return (
@@ -110,6 +124,7 @@ function App() {
               <h2>PhySims</h2>
             </div>
             <br />
+            <h3>Overview</h3>
             <p>
               Physics simulations are common teaching tools for introductory
               physics teachers everywhere. They help students learn about
@@ -122,14 +137,86 @@ function App() {
             </p>
             <br />
             <p>
-              I developed the physics simulation platform, PhySims, to bridge
-              this gap, allowing students a choice between freeform mode, review
-              mode, and tutorial mode to learn various introductory mechanics
-              topics.
+              As an independent study project, I developed the physics
+              simulation platform, PhySims, to bridge this gap, allowing
+              students a choice between freeform mode, review mode, and tutorial
+              mode to learn various introductory mechanics topics in
+              algebra-based physics courses.
             </p>
             <br />
-            <h3>Freeform Mode</h3>
+            <h3>Research</h3>
+            <p>
+              {" "}
+              I started by speaking with other students to understand what their
+              needs were. I spoke to both students taking college-level physics
+              and students that had only taken high school level physics, since
+              the goal of this project was to provide an introduction to
+              algebra-based mechanics, which can be taken at both levels.
+            </p>
             <br />
+            <p>
+              Next, I completed a survey of existing work in this area,
+              critically reviewing existing simulations from PhET, Algodoo, and
+              other platforms, noting both features that I would want to
+              incorporate in my own project and where they were lacking. I also
+              reviewed research papers in this area and spoke to professors who
+              had done work in the field.{" "}
+            </p>
+            <br />
+            <h3>Planning</h3>
+            <p>Two major principles guided my design process:</p>
+            <ul>
+              <li>
+                <b>Extensibility:</b> In structuring both the user interface and
+                the code, my goal was to have an extensible design that would be
+                easy to add additional features and types of problems to.{" "}
+              </li>
+              <li>
+                <b>Flexibility</b>: My goal was to receive frequent feedback so
+                that I could quickly change direction if something wasn&apos;t
+                working out. I made a point of receiving feedback at every step
+                of the design project, and continuously iterated on my design
+                based on that feedback.
+              </li>
+            </ul>
+            <br />
+            <p>
+              My original design was focused on creating interactive, physically
+              correct simulations where students could set the parameters. After
+              receiving initial feedback, I decided it would also be beneficial
+              to also allow for students to control the forces acting on
+              elements in the simulation in order to practice concepts. After
+              receiving feedback on the modified design, I realized it would
+              also be helpful to have a way of showing students how to correctly
+              solve problems.
+            </p>
+            <br />
+            <p>
+              {" "}
+              It quickly became clear that it would be impossible to accomplish
+              everything I wanted to accomplish with a single mode, so I shifted
+              focus to a three-mode model:{" "}
+            </p>
+            <ul>
+              <li>
+                <b>Freeform mode</b> would be used for students to get an
+                initial understanding of how kinematics work by setting up their
+                own problems that would be animated physically correctly
+              </li>
+              <li>
+                <b>Tutorial mode</b> would be used to walk students through
+                concepts like drawing free body diagrams and solving for various
+                parameters
+              </li>
+              <li>
+                <b>Review mode</b> would be used for students to practice their
+                problem solving skills by solving semi-randomly generated
+                problems
+              </li>{" "}
+            </ul>
+            <br />
+            <h3>Designing</h3>
+            <h4>Freeform Mode</h4>
             <p>
               In Freeform Mode, students can explore pendulum problems, incline
               plane problems, and more, toggling parameters and running
@@ -139,8 +226,7 @@ function App() {
             </p>
             insert gif of pendulum with force/acc/vel visible
             <br />
-            <h3>Tutorial Mode</h3>
-            <br />
+            <h4>Tutorial Mode</h4>
             <p>
               In Tutorial Mode, students receive step-by-step guidance on how to
               solve different categories of problems, with visual depictions and
@@ -148,8 +234,7 @@ function App() {
             </p>
             insert gif going through steps of solving incline plane problem
             <br />
-            <h3>Review Mode</h3>
-            <br />
+            <h4>Review Mode</h4>
             <p>
               In Review Mode, students can practice their problem-solving skills
               by solving randomly generated problems. Students can sketch out
