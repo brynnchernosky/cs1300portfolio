69fa32a8339a43d1a27bd3c75c2933d6e3259aa0
brynnchernosky <56202540+brynnchernosky@users.noreply.github.com>
2022-12-05 13:44:17 -0500

    refactor

---
+++ src/CraigslistSection.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import Carousel from "react-material-ui-carousel";
+import { Paper } from "@mui/material";
+import { Section } from "./Section";
+
+export const CraigslistSection = () => {
+  return (
+    <Section
+      title={""}
+      titleBackgroundImage={""}
+      firstSectionContent={<div></div>}
+      firstSectionCarousel={null}
+      secondSectionTitle={""}
+      secondSectionContent={<div></div>}
+      thirdSectionTitle={""}
+      thirdSectionContent={<div></div>}
+      finalDesignContent={<div></div>}
+      takeawaysContent={<div></div>}
+    />
+  );
+};
